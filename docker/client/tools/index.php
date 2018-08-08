ef71b7a954a95be3d532b1910862675f0b646a67
liweipei <778323309@qq.com>
2018-08-08 20:00:46 +0800

    20180808200046

--- docker/client/tools/index.php
+++ docker/client/tools/index.php
@@ -2,7 +2,7 @@
 $client = new swoole_client(SWOOLE_SOCK_TCP);
 
 //连接到服务器
-!$client->connect("server", 91, 0.5) and  die("connect failed.");
+!$client->connect("server", 90, 0.5) and  die("connect failed.");
 
 //向服务器发送数据
 if (!$client->send("hello world")) die("send failed.");
