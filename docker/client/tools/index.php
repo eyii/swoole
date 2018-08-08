772d543691dd093370c68b51bde262763ed16792
liweipei <778323309@qq.com>
2018-08-08 17:53:13 +0800

    20180808175312

--- docker/client/tools/index.php
+++ docker/client/tools/index.php
@@ -1,5 +1,5 @@
 <?php
-$client = new swoole_client(SWOOLE_SOCK_TCP, SWOOLE_SOCK_ASYNC);
+$client = new swoole_client(SWOOLE_SOCK_TCP, SWOOLE_SOCK_SYNC);
 $client->on("connect", function($cli) {$cli->send("hello world\n");});
 $client->on("receive", function($cli, $data){echo "received: {$data}\n";});
 $client->on("error", function($cli){echo "connect failed\n";});
