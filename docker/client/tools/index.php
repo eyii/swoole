4a6249364dbd8acc4e1498d79f3ffa53273303d5
liweipei <778323309@qq.com>
2018-08-08 19:40:36 +0800

    20180808194035

--- docker/client/tools/index.php
+++ docker/client/tools/index.php
@@ -1,8 +1,2 @@
 <?php
-var_dump(1);
-$client = new swoole_client(SWOOLE_SOCK_TCP);
-if (!$client->connect("server", 90, -1)) exit("connect failed. Error: {$client->errCode}\n");
-
-$client->send("hello world\n");
-echo $client->recv();
-$client->close();
+exit(111111111111);
