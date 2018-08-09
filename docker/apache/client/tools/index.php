a2d7865d07f1ddf8ad78e1942cee3a41bc091b47
liweipei <778323309@qq.com>
2018-08-09 17:15:59 +0800

    20180809171559

--- docker/apache/client/tools/index.php
+++ docker/apache/client/tools/index.php
@@ -1,6 +1,6 @@
 <?php
 $client = new swoole_client(SWOOLE_SOCK_TCP);
-!$client->connect("server", 90, 0.5) and  die("connect failed.");
+!$client->connect("server", 91, 0.5) and  die("connect failed.");
 if (!$client->send("hello world")) die("send failed.");
 $data = $client->recv();
 if (!$data) die("recv failed.");
--- docker/apache/server/tools/index.php
+++ docker/apache/server/tools/index.php
@@ -1,5 +1,5 @@
 <?php
-$server = new swoole_server("0.0.0.0", 90);
+$server = new swoole_server("0.0.0.0", 91);
 $server->on('connect', function ($serv, $fd) {echo "Client: Connect.\n";});
 $server->on('receive', function ($serv, $fd, $from_id, $data) {$serv->send($fd, "Server: ".$data);});
 $server->on('close', function ($serv, $fd) {echo "Client: Close.\n";});
